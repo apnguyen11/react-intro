a788b47b306c44ae308c9277618180fad4c90643
Andy Nguyen <apnguyen11@yahoo.com>
2019-11-16 11:35:19 -0600

    react notes about components and class

--- app.js
+++ app.js
@@ -16,6 +16,52 @@ const List = function(props){
         </div>
     )
 }
+
+
+//starting out count vall is zero
+
+class Counter extends React.Component{
+    state={
+        count:0,
+        color: "blue"
+    }
+    handleAdd= ()=>{
+        this.setState({
+            count:this.state.count +1
+            
+        }
+            
+        )
+    }
+    handleToggle=()=>{
+        // const color1 = (this.state.color === 'blue')?"green" : "blue";
+
+        let color = "blue"
+        if(this.state.color === 'blue'){
+            color = 'green'
+        } 
+
+        this.setState({
+            color:color
+        })
+
+    }
+    render(){
+        console.log('this render step in the lifecylce is happening', this.state)
+        return(
+            <div>
+                <h1 style={{
+                    color: this.state.color
+                }}>Hello{this.props.name}</h1>
+                <p>The count is {this.state.count}</p>
+                <button onClick = {this.handleToggle}>Toggle</button>
+                <button onClick={this.handleAdd}>Add</button>
+            </div>
+            
+        )
+
+    }
+}
 const Hello = function(props){
     return(
         <div>
@@ -26,13 +72,26 @@ const Hello = function(props){
         </div>
     )
 }
+// const App = function(){
+//     const component = new HelloClass({name: 'joey is cool'})
+//     return (
+//         <div>
+//             <p>functional example</p>
+//             <Hello name="WaterMeolon"/>
+//             <HelloClass name="Banana"/>
+//             <p>class example</p>
+//             {new HelloClass({name: "New instantiated banana"}).render()}
+//             {component.render()}
+//         </div>
+//     )
+// }
 const App = function(){
+   
     return (
         <div>
-            <Hello name="WaterMeolon"/>
-            <HelloClass name="Banana"/>
-            {new HelloClass({name: "New instantiated banana"}).render()}
-          
+            <Counter name="first counter"/>
+            <Counter name="first counter"/>
+            <Counter name="first counter"/>
         </div>
     )
 }
@@ -44,6 +103,13 @@ const App = function(){
 // }
 
 class HelloClass extends React.Component{
+    componentDidMount(){
+        //methods in react 
+        console.log("A helloclass component just mounted with propes", this .props)
+    }
+    componentWillUnmount(){
+        console.log("this is the component about to be unmounted")
+    }
     render(){
         return(
             <div>

5d8255c74b806e730cb3f74b3399b94089b31cbb
Andy Nguyen <apnguyen11@yahoo.com>
2019-11-16 10:55:21 -0600

    new react examples

--- app.js
+++ app.js
@@ -2,5 +2,60 @@ function HelloWorld(){
     return <h1> Hello World</h1>
 }
 
-ReactDOM.render(<HelloWorld/>, document.getElementById('root'))
+// function App(){
+
+// }
+
+const List = function(props){
+    return (
+        <div>
+            <p>This {props.name}'s list of things to do</p>
+            <ul>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
+const Hello = function(props){
+    return(
+        <div>
+            <h1>
+                Hello {props.name}
+            </h1>
+            <List name={props.name}/>
+        </div>
+    )
+}
+const App = function(){
+    return (
+        <div>
+            <Hello name="WaterMeolon"/>
+            <HelloClass name="Banana"/>
+            {new HelloClass({name: "New instantiated banana"}).render()}
+          
+        </div>
+    )
+}
+
+// const App = () => {
+//     return <div className="container">
+//         <HelloWorld/>
+//     </div>
+// }
+
+class HelloClass extends React.Component{
+    render(){
+        return(
+            <div>
+            <h1>
+                Hello {this.props.name}
+            </h1>
+            <List name={this.props.name}/>
+        </div>
+        )
+    }
+}
+
+
+ReactDOM.render(<App/>, document.getElementById('root'))
 
